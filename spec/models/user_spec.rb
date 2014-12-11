95d076670001f70fb1a8ef74834c15a113314cdb
jatin <jatin@browserstack.com>
2014-12-11 13:46:52 +0530

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,30 +3,40 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
 
   before(:each) do
-    @user = FactoryGirl.create(:user)
+    @user = create(:user)
+    @user_attr = attributes_for(:user)
   end
 
-  it "generates a hash on creation" do
-    f = FactoryGirl.create(:user2)
+  it "should create a user with valid attributes" do
+    f = create(:user2)
     expect(f).to be_a(User)
   end
 
   it "should not allow duplicate emails" do
     expect do
-      f = FactoryGirl.create(:user)
-    end.to raise_error("Validation failed: Email has already been taken")
+      f = create(:user)
+    end.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email has already been taken")
   end
 
   it "should require email" do
-
+    expect do
+      @user_attr[:email] = ''
+      f = User.create!(@user_attr)
+    end.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it "should require password" do
-
+    expect do
+      @user_attr[:password] = ''
+      f = User.create!(@user_attr)
+    end.to raise_error(ActiveRecord::RecordInvalid)
   end
 
   it "should ensure that password_confirmation is the same" do
-
+    expect do
+      @user_attr[:password_confirmation] = 'asdsa'
+      f = User.create!(@user_attr)
+    end.to raise_error(ActiveRecord::RecordInvalid)
   end
 
 end
