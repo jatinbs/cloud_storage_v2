8673a9d50ec9cd3a6207b85d503b693fbc3d84e3
jatin <jatin@browserstack.com>
2014-12-12 16:26:15 +0530

    added downloads_controller_spec_file. fixed some routing specs

---
+++ spec/controllers/downloads_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe DownloadsController, :type => :controller do
+
+
+
+
+  describe "GET #download_file" do
+
+    before(:each) do
+    end
+
+    it "starts download of a file" do
+      # f = create(:file_upload)
+      # post :download_file, {file_hash: f.file_hash}
+      # expect(response).to be_success
+      # expect(controller).to receive(:send_file)
+    end
+  end
+
+  describe "POST #download_page" do
+
+  end
+
+end
--- spec/routing/file_uploads_routing_spec.rb
+++ spec/routing/file_uploads_routing_spec.rb
@@ -15,16 +15,8 @@ RSpec.describe FileUploadsController, :type => :routing do
       expect(:get => "/file_uploads/1").to route_to("file_uploads#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/file_uploads/1/edit").to route_to("file_uploads#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/file_uploads").to route_to("file_uploads#create")
-    end
-
-    it "routes to #update" do
-      expect(:put => "/file_uploads/1").to route_to("file_uploads#update", :id => "1")
     end
 
     it "routes to #destroy" do
