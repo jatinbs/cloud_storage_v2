ef2585b8acbfc6b56ec1264d382324a5bb1dc4f1
jatin <jatin@browserstack.com>
2014-12-12 19:41:17 +0530

    file upload request flows.

--- spec/requests/file_uploads_spec.rb
+++ spec/requests/file_uploads_spec.rb
@@ -1,10 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe "FileUploads", :type => :request do
+RSpec.describe "FileUploads", :type => :feature do
+
   describe "GET /file_uploads" do
-    it "works! (now write some real specs)" do
-      get file_uploads_path
-      expect(response).to have_http_status(200)
+
+    before :each do
+      @user ||= FactoryGirl.create :user
+      visit user_session_path
+      fill_in "user[email]", with: @user.email
+      fill_in 'user[password]', with: @user.password
+      click_button 'Log in'
+    end
+
+    it "works!" do
+      visit file_uploads_path
+      expect(page.status_code).to eq(200)
     end
   end
+
+  describe "create_file_upload" do
+
+    before :each do
+      @user ||= FactoryGirl.create :user
+      visit user_session_path
+      fill_in "user[email]", with: @user.email
+      fill_in 'user[password]', with: @user.password
+      click_button 'Log in'
+    end
+
+    it "successfully uploads a file qnd then can delete it" do
+      visit new_file_upload_path
+      expect(page.status_code).to eq(200)
+      attach_file "file_upload[file]", Rails.root.join('spec', 'test_uploads', 'test2.jpg')
+      click_button "Save File"
+      expect(page).to have_content('test2.jpg')
+      expect(page).to have_selector('a', text: 'Download')
+    end
+
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment",__FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'capybara/rspec'
 require 'devise'
 require_relative 'support/controller_macros'
 include ActionDispatch::TestProcess
